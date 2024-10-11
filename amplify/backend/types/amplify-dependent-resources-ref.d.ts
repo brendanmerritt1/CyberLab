c8db23404934710cffa4e777f0ba75e8ce96b326
Brendan Merritt <brendanmerritt1@gmail.com>
2024-10-11 14:47:34 -0400

    Amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cyberlab9ede7647": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s39f629687": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
