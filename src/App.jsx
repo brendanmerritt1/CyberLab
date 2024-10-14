34b901e00c672f6459e1d3a4f0a0d03129228d2c
Brendan Merritt <brendanmerritt1@gmail.com>
2024-10-14 14:05:45 -0400

    Amplify backend deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.json';
 import Homepage from './pages/homepage/Homepage.jsx';
-
-Amplify.configure(outputs);
 
 function App() {
   return <Homepage />;
